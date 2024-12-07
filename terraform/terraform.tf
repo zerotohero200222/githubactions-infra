60cc566dcf8cf9416ea9fedf9d552c391fd4384f
Ndzenyuy <ndzenyuyjones@gmail.com>
2024-12-08 00:12:41 +0100

    infrastructure code

---
+++ terraform/terraform.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "rhenaactions"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+
+  #required_version = "~> 1.5.1"
+  required_version = "~> 1.7.0"
+
+}
+##
+##
+
---
+++ terraform/vpc.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "rhena-eks"
+
+  cidr = "172.20.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3) # Gives the name of the availability zones in the specified region, from 0-3 only
+
+  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
+  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
